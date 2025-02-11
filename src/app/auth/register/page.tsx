51805ee952525916c91e9f12c312c389916aac96
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-02-11 11:53:28 +0100

    feat: Add registration page with role selection

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import { title } from 'process';
+
+export const metadata = {
+    title: "Get Started | Create your Blue Sands Account"
+}
+
+const linkOptions = [
+    {
+        title: "Individual",
+        url: "/auth/register/individual"
+    },
+    {
+        title: "School",
+        url: "/auth/register/school"
+    },
+]
+
+export default function RegisterUser() {
+    return (
+        <section className="min-h-screen p-3">
+            <div className='w-full flex justify-center relative z-0'>
+                <img src="/images/bg/cover.png" className="w-full object-contain z-0" alt="" />
+                <div className="absolute text-center text-white max-w-lg md:bottom-28 bottom-5 space-y-3">
+                    <h1 className="text-2xl md:text-5xl font-normal">Get Started</h1>
+                    <p className="font-thin md:text-xl text-sm md:max-w-lg max-w-xs">Choose your role to begin your journey on Blue Sands STEM Labs.</p>
+                </div>
+            </div>
+            <div className='border max-w-lg mx-auto flex flex-col gap-y-5 py-20 px-5 rounded-lg shadow-sm mt-0 md:-mt-20 z-30 relative bg-white'>
+                {linkOptions.map((link, index) => (
+                    <Link href={link.url} key={index} className={`text-center  rounded-md py-5 ${index === 0 ? "bg-bgBlue text-white" : "bg-bgGrey"} `}>{link.title}</Link>
+                ))}
+            </div>
+        </section>
+    )
+}
