44dfbdb3854fc52e95488bc6c66f5723a05cab99
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-02-12 22:11:01 +0100

    feat: Add loading bar component for page navigation

---
+++ src/components/LoadingBar/index.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { useEffect } from "react";
+import NProgress from "nprogress";
+import { usePathname, useSearchParams } from "next/navigation";
+
+export default function LoadingBar() {
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        NProgress.done();
+        return () => {
+            NProgress.start();
+        };
+    }, [pathname, searchParams]);
+
+    return null;
+}
