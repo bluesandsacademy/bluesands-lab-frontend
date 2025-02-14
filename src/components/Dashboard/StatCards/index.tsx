9e7fb62ec0ade0bc6dadae08b2f21f0ab8fd8fca
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-02-14 17:19:24 +0100

    feat: create StatCards component for dashboard statistics display

---
+++ src/components/Dashboard/StatCards/index.tsx
@@ -0,0 +1,30 @@
+import { Stat } from "@/lib/data"
+
+export default function Stats({ stats }: any) {
+    return (
+        <section className="w-full grid grid-cols-4 gap-4">
+            {stats.map((stat: Stat, index: number) => {
+                return (
+                    <div key={index} className="border p-5 rounded-2xl shadow bg-white">
+                        <div className="flex justify-between">
+                            <div className="space-y-1">
+                                <h3 className="text-md text-gray-600 font-medium">{stat.title}</h3>
+                                <p className="text-xl font-medium">{stat.percentage}</p>
+                            </div>
+                            <div>
+                                <img src={stat.icon} alt="" />
+                            </div>
+                        </div>
+                        <div>
+                            <img src={stat.trendIcon} alt="" />
+                        </div>
+                        <div className="flex justify-start ml-5">
+                            <p className="text-green-600 font-medium text-lg">{stat.percentageChange} <span className="text-gray-400">Up {stat.timeFrame
+                            }</span></p>
+                        </div>
+                    </div>
+                )
+            })}
+        </section>
+    )
+} 
