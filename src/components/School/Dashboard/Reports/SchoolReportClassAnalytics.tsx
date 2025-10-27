8b2b21a4e7cb88fbe9232c47d236677178522b2d
Chibueze <arisachibueze@gmail.com>
2025-10-27 16:52:14 +0100

    added user and school management pages in global admin

--- src/components/School/Dashboard/Reports/SchoolReportClassAnalytics.tsx
+++ src/components/School/Dashboard/Reports/SchoolReportClassAnalytics.tsx
@@ -45,36 +45,38 @@ const SchoolReportClassAnalytics = () => {
       <p className="text-sm font-semibold text-[#003C6E] ml-4">
         Detailed Class Metrics
       </p>
-      <table className="bg-white rounded-md">
-        <thead>
-          <tr className="border-b border-b-gray-200 text-xs text-gray-500">
-            <td className="p-2">Class</td>
-            <td className="p-2">Students</td>
-            <td className="p-2">Average Score</td>
-            <td className="p-2">Attendance</td>
-            <td className="p-2">Status</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="text-xs border-b border-b-gray-200">
-            <td className="p-2">Class Name</td>
-            <td className="p-2">student no</td>
-            <td className="p-2">
-              <p className="flex w-max p-0.5 px-2 bg-green-200 text-green-600 items-center justify-center rounded-md">
-                {" "}
-                28
-              </p>
-            </td>
-            <td className="p-2">14</td>
-            <td className="p-2">
-              <p className="flex w-max p-0.5 px-2 bg-green-200 text-green-600 items-center justify-center rounded-md">
-                {" "}
-                Excellent
-              </p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="flex flex-col overflow-x-scroll">
+        <table className="bg-white rounded-md">
+          <thead>
+            <tr className="border-b border-b-gray-200 text-xs text-gray-500">
+              <td className="p-2">Class</td>
+              <td className="p-2">Students</td>
+              <td className="p-2">Average Score</td>
+              <td className="p-2">Attendance</td>
+              <td className="p-2">Status</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="text-xs border-b border-b-gray-200">
+              <td className="p-2">Class Name</td>
+              <td className="p-2">student no</td>
+              <td className="p-2">
+                <p className="flex w-max p-0.5 px-2 bg-green-200 text-green-600 items-center justify-center rounded-md">
+                  {" "}
+                  28
+                </p>
+              </td>
+              <td className="p-2">14</td>
+              <td className="p-2">
+                <p className="flex w-max p-0.5 px-2 bg-green-200 text-green-600 items-center justify-center rounded-md">
+                  {" "}
+                  Excellent
+                </p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
